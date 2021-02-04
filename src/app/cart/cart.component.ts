629f82712a364ac88380eff6a19838d195feebed
manjunath-muniraju <manjunathm3@gmail.com>
2021-02-04 12:07:41 +0530

    day4 - part2 - linking error pages - two ways

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../cart.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-
   items = this.cartService.getItems();
-  
-  constructor(
-    private cartService: CartService
-  ) { }
 
-  ngOnInit() {
+  constructor(private cartService: CartService, private router: Router) {}
+
+  ngOnInit() {}
+
+  routeToPNF() {
+    this.router.navigateByUrl("page-not-found");
   }
-
 }
